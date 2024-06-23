925ae58e415874604372f593d2db5b323d1006dd
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2024-06-23 10:24:08 +0530

    Add files via upload

---
+++ calculater.py
@@ -0,0 +1,45 @@
+# import addation
+# print(addation.add(23,34))
+
+# #import with renaming 
+# import addation 
+# print(addation.add(3,5))
+
+# import addation as a 
+# print(a.add(4,5))
+
+# import addation as a
+# print(a.sum(2,4))
+
+# import addation as a 
+# print(a.sub(2,4))
+# print(a.mul(3,4))
+# print(a.div(2,3))
+
+# # there  are two types  of modules in python 
+# # user defined modules :- modules create by us 
+# # built in modules :- modules given by python 
+# import math 
+# print(math.factorial(5))
+
+# import math as m 
+# print(m.factorial(45))
+
+import addation 
+import addation 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
